f4383323cb88de2a104fae94df755a074b8542f5
Mauro Crispim <mcrispim@hotmail.com>
2023-07-21 17:25:56 -0300

    Day13

--- 2015/Day13/src/main/kotlin/Main.kt
+++ 2015/Day13/src/main/kotlin/Main.kt
@@ -28,74 +28,92 @@ fun generateSequence(A: List<String>): Sequence<List<String>> {
     }
 }
 
+fun addHostToPrefsAndPeople() {
+    for (p in people) {
+        prefs[Pair(p, "Host")] = 0
+        prefs[Pair("Host", p)] = 0
+    }
+    people.add("Host")
+}
+
+fun mountPrefsAndPeople(input: List<String>) {
+    val everybody = mutableSetOf<String>()
+    prefs.clear()
+    input.forEach {
+        val parts = it.split(" ")
+        val name1 = parts[0]
+        val name2 = parts[10].dropLast(1)
+        val value = if (parts[2] == "gain")
+            parts[3].toInt()
+        else
+            -(parts[3].toInt())
+        prefs[Pair(name1, name2)] = value
+        everybody.add(name1)
+        everybody.add(name2)
+    }
+    people = everybody.toMutableList()
+}
+
+fun happyness(people: List<String>): Int {
+    var happyness = 0
+    happyness += prefs[Pair(people.last(), people.first())]!!
+    happyness += prefs[Pair(people.first(), people.last())]!!
+
+    for ((personA, personB) in people.zipWithNext()) {
+        happyness += prefs[Pair(personA, personB)]!!
+        happyness += prefs[Pair(personB, personA)]!!
+    }
+    return happyness
+}
+
+fun calculateHappyness(table: Sequence<List<String>>): Int {
+    var happyness = 0
+    table.forEachIndexed { index, people ->
+        val normalizedPeople = normalizePeople(people)
+        if (combinations.containsKey(normalizedPeople)) {
+            return@forEachIndexed
+        }
+        val thisHappyness = happyness(normalizedPeople)
+        combinations[normalizedPeople] = thisHappyness
+        if (thisHappyness > happyness) {
+            happyness = thisHappyness
+        }
+    }
+    return happyness
+}
+
+fun normalizePeople(peopleAtTable: List<String>): List<String> {
+    val firstPerson = people.first()
+    val firstIndex = peopleAtTable.indexOf(firstPerson)
+    val result = peopleAtTable.subList(firstIndex, peopleAtTable.size) +
+            peopleAtTable.subList(0, firstIndex)
+    return result
+}
+
 val prefs = mutableMapOf<Pair<String, String>, Int>()
-var people = listOf<String>()
+var people = mutableListOf<String>()
+val combinations = mutableMapOf<List<String>, Int>()
 
 fun main() {
 
-    // Alice would gain 54 happiness units by sitting next to Bob.
-    fun mountPrefsAndPeople(input: List<String>) {
-        val everybody = mutableSetOf<String>()
-        prefs.clear()
-        input.forEach {
-            val parts = it.split(" ")
-            val name1 = parts[0]
-            val name2 = parts[10].dropLast(1)
-            val value = if (parts[2] == "gain")
-                parts[3].toInt()
-            else
-                -(parts[3].toInt())
-            prefs[Pair(name1, name2)] = value
-            everybody.add(name1)
-            everybody.add(name2)
-        }
-        people = everybody.toList()
+    fun findMostHappynnes(): Int {
+        val table: Sequence<List<String>> = generateSequence(people)
+        return calculateHappyness(table)
     }
 
-    fun CalculateHappyness(people: List<String>): Int {
-        var happyness = 0
-        happyness += prefs[Pair(people.last(), people.first())]!!
-        happyness += prefs[Pair(people.first(), people.last())]!!
-
-        for ((personA, personB) in people.zipWithNext()) {
-            happyness += prefs[Pair(personA, personB)]!!
-            happyness += prefs[Pair(personB, personA)]!!
-        }
-        return happyness
-    }
-
-    fun part1(input: List<String>): Int {
-        val table = generateSequence(people)
-
-        var happyness = 0
-        var group = listOf<String>()
-        table.forEach {
-            val thisHappyness = CalculateHappyness(it)
-            println("$it $thisHappyness")
-            if (thisHappyness > happyness) {
-                happyness = thisHappyness
-                group = it
-            }
-        }
-        return happyness
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day13_test")
     mountPrefsAndPeople(testInput)
-
-
-    val part1Result = part1(testInput)
+//    printPrefsAndPeople()
+    var part1Result = findMostHappynnes()
     check(part1Result == 330)
-    check(part2(testInput) == 12)
 
     val input = readInput("Day13_data")
-    println("Part 1 answer: ${part1(input)}")
-    println("Part 2 answer: ${part2(input)}")
+    mountPrefsAndPeople(input)
+    part1Result = findMostHappynnes()
+    println("Part 1 answer: $part1Result")
+    addHostToPrefsAndPeople()
+    val part2Result2 = findMostHappynnes()
+    println("Part 2 answer: $part2Result2")
 }
 
 /**
